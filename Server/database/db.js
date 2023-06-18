b42c1d721381b6b4806f10e04d21d761676ecda8
Aacini++ <38142351+Aacini-pp@users.noreply.github.com>
2023-06-18 06:24:59 -0600

    env bd
    env db

--- Server/database/db.js
+++ Server/database/db.js
@@ -1,7 +1,7 @@
 import  {Sequelize} from 'sequelize'
-const db =  new Sequelize("sysFAB","usersysfab","Ao^KdxZiq5Pik&m4Ñ",{
-    host: 'localhost',
-    port: '8696',
+const db =  new Sequelize(process.env.DB_NAME ,process.env.DB_USER ,process.env.DB_PASS ,{
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT ,
     dialect:"mysql",
     logging: false
 
